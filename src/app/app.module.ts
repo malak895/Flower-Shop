59181fef5422ef799ef24906affe2ef9f3723ca8
malak895 <malak.hammami263@gmail.com>
2023-08-04 14:05:36 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { PaymentComponent } from './payment/payment.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    PaymentComponent
+    PaymentComponent,
+    FooterComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
